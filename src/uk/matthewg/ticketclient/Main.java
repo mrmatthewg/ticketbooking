51009870d496c9ec5812002848e39eb64fc49a73
mrmjg <matthew.john.gardner@gmail.com>
2014-11-06 14:22:50 +0000

    Added test buttons to the main class.

--- src/uk/matthewg/ticketclient/Main.java
+++ src/uk/matthewg/ticketclient/Main.java
@@ -1,8 +1,27 @@
 package uk.matthewg.ticketclient;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        /* First frame */
+        JFrame topframe = new JFrame("Book tickets now!");
+        JButton searchButton = new JButton("Search");
+        JButton aButton = new JButton("a");
+
+        topframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        topframe.getContentPane().add(searchButton, BorderLayout.CENTER);
+
+        topframe.getContentPane().add(aButton, BorderLayout.PAGE_START);
+
+        topframe.pack();
+
+        topframe.setVisible(true);
+
     }
+
 }
